64f9716de300f58a79ce36405f9abdefc46a9bb2
dankorotenko <danilkorotenko2@gmail.com>
2023-01-31 18:52:10 +0100

    ptls-160 fix

--- src/components/Triggers.jsx
+++ src/components/Triggers.jsx
@@ -1,45 +1,17 @@
 import React, { useEffect, useState } from "react";
 
-import Account from "./icons/logos/Account";
-import Aptos from "./icons/logos/Aptos";
-import Wallet from './icons/logos/Account'
-import Topaz from "./icons/logos/Topaz";
+import { cards} from '../data/dummy'
+
 import TriggerCard from "./TriggerCard";
 
 export default function Triggers({ handleChildProps }) {
   const [open, setOpen] = useState(null);
   const [closeCard, setCloseCard] = useState(null);
 
-  const cards = [
-    {
-      title: "Account",
-      desc: "Track Account Liquidity",
-      img: <Account />,
-    },
-    {
-      title: "Aptos",
-      desc: "Track APT price",
-      img: <Aptos />,
-    },
-    {
-      title: "Wallet",
-      desc: "Track new transaction",
-      img: <Wallet />,
-    },
-    {
-      title: "Topaz",
-      desc: "Track NFT floor price",
-      img: <Topaz />,
-    },
-  ];
 
   const handleOpen = (i) => {
     setOpen(i);
     handleChildProps({ trigger: cards[0].title.toLowerCase() });
-  };
-
-  const handleClose = (i) => {
-    setCloseCard(i);
   };
 
   useEffect(() => {
@@ -57,8 +29,7 @@ export default function Triggers({ handleChildProps }) {
           isOpen={i === open}
           isHidden={i !== open && open !== null}
           onClick={() => handleOpen(i)}
-          handleClose={handleClose}
-          setOpen={setOpen}
+          setCloseCard={setCloseCard}
         />
       ))}
     </div>
